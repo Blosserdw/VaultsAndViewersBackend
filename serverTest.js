48292d1d6db0c27cbea5b881e4d7f43fa7b06238
David Blosser <blosserdw@gmail.com>
2018-04-24 15:37:24 -0400

    adding origins for socket.io CORS stuff

--- serverTest.js
+++ serverTest.js
@@ -20,29 +20,12 @@ const PORT = process.env.PORT || 3000;
 const INDEX = path.join(__dirname, 'index.html');
 
 const server = express()
-  .use(function(req, res, next)
-	  {
-		res.sendFile(INDEX);
-		
-		 // Website you wish to allow to connect
-		res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8012/index.html');
-
-		// Request methods you wish to allow
-		res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-		// Request headers you wish to allow
-		res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-		// Set to true if you need the website to include cookies in the requests sent
-		// to the API (e.g. in case you use sessions)
-		res.setHeader('Access-Control-Allow-Credentials', true);
-
-		// Pass to next layer of middleware
-		next();
-	  })
+  .use((req, res) => res.sendFile(INDEX) )
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 const io = socketIO(server);
+
+io.origins('*:*');
 
 
 
